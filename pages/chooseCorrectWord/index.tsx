48e8bfa7a22012f6c6d1d9bfeae0593a4082d10d
juanpa77 <juanpa.box.77@gmail.com>
2022-06-12 10:24:24 -0300

    Add new page with new mode Choose Correct word

---
+++ pages/chooseCorrectWord/index.tsx
@@ -0,0 +1,22 @@
+import AppLayout from '../../components/AppLayout'
+import MoleWord from '../../components/Word'
+import useWords from '../../hooks/useWords'
+import { WrapperWords } from '../learnToRead/styled'
+
+export default function ChooseCorrectWord () {
+  const optionNumber = 4
+
+  const [word, nextWord, isOnScreen, offScreen] = useWords()
+
+  return (
+    <AppLayout>
+      <WrapperWords>
+        {[...Array(optionNumber)].map((i) => {
+          return (
+            <MoleWord key={i} word={word} area={''} isOnScreen={true} />
+          )
+        })}
+      </WrapperWords>
+    </AppLayout>
+  )
+}
